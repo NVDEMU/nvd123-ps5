c469f9e53ce30aa935a57da2c3c5454210112d56
NVDEMU <ndelpo318@gmail.com>
2026-09-22 12:17:40 -0400

    Point NVDS5 project links at NVDS5

--- src/SharpEmu.GUI/ProjectLinks.cs
+++ src/SharpEmu.GUI/ProjectLinks.cs
@@ -1,11 +1,11 @@
-// Copyright (C) 2026 SharpEmu Emulator Project
+// Copyright (C) 2026 NVDS5 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 namespace SharpEmu.GUI;
 
 internal static class ProjectLinks
 {
-    private const string RepositorySlug = "sharpemu/sharpemu";
+    private const string RepositorySlug = "NVDEMU/nvd123-ps5";
 
     public static string RepositoryUrl => $"https://github.com/{RepositorySlug}";
 
